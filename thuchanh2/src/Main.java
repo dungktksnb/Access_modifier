8a05b009fc488a9f2b9366861150f762a84855cb
dungktksnb <dungcnttk14g@gmail.com>
2021-06-11 08:51:56 +0700

    th2

---
+++ thuchanh2/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Car car1=new Car("mazda 3","Sky 3");
+        System.out.println(car1.numberOfCar);
+        Car car2=new Car("mazda 6","Sky 6");
+        System.out.println(car2.numberOfCar);
+    }
+}
