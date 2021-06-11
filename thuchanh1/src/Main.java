2b1e1df73893d24a3d9c9b502cc85c691bc09a3c
dungktksnb <dungcnttk14g@gmail.com>
2021-06-11 08:36:09 +0700

    baithuchanh

---
+++ thuchanh1/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Student.change();
+        Student s1=new Student(111,"tuấn");
+        Student s2=new Student(222,"dũng");
+        Student s3=new Student(333,"hòa");
+        s1.display();
+        s2.display();
+        s3.display();
+    }
+}
---
+++ thuchanh1/src/Student.java
@@ -0,0 +1,37 @@
+public class Student {
+    private int rollno;
+    private String name;
+    private static String college = "BBDIT";
+
+    public Student(int rollno, String name) {
+        this.rollno = rollno;
+        this.name = name;
+    }
+
+    public int getRollno() {
+        return rollno;
+    }
+
+    public void setRollno(int rollno) {
+        this.rollno = rollno;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public static String getCollege() {
+        return college;
+    }
+
+    public static void change() {
+        Student.college = "CODEGYM";
+    }
+    void display(){
+        System.out.println(this.rollno + " " + this.name + " "+this.college );
+    }
+}
